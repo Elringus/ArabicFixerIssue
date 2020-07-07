8682e652c58a4757313aa4faa84650d696cd5ea0
Artyom Sovetnikov <elringus@yandex.ru>
2020-07-07 10:50:25 +0300

    Initial commit

---
+++ Assets/Runtime/SetArabicText.cs
@@ -0,0 +1,16 @@
+﻿using ArabicSupport;
+using TMPro;
+using UnityEngine;
+
+public class SetArabicText : MonoBehaviour
+{
+    [TextArea]
+    public string Message;
+    public TextMeshProUGUI Text;
+
+    private void Start ()
+    {
+        var fixedMessage = ArabicFixer.Fix(Message, Text.isRightToLeftText);
+        Text.text = fixedMessage;
+    }
+}
