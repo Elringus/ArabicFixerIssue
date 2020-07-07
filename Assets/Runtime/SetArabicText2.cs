0fb8bf4355dead222c8ceadf439e63d9eac014ab
Ahmed Ashour <asashour@yahoo.com>
2020-07-07 15:12:44 +0200

    Test Arabic with Text

---
+++ Assets/Runtime/SetArabicText2.cs
@@ -0,0 +1,26 @@
+﻿using ArabicSupport;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetArabicText2 : MonoBehaviour
+{
+    [TextArea]
+    public string Message;
+    public TextMeshProUGUI Text;
+    public Text simpleText;
+
+    private void Start()
+    {
+        var fixedMessage = ArabicFixer.Fix(Message);
+        //fixedMessage = "أهلا";
+        if (Text)
+        {
+            Text.text = fixedMessage;
+        }
+        if (simpleText)
+        {
+            simpleText.text = fixedMessage;
+        }
+    }
+}
